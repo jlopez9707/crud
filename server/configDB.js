47f6ce6bc900a0a55e810a72ca627aea3d9d40b8
Javier Lopez <javierlopezrequejo@gmail.com>
2024-01-31 08:48:50 -0500

    add .env file

--- server/configDB.js
+++ server/configDB.js
@@ -1,18 +1,20 @@
 //Conectar a base de datos SQL Server
+require('dotenv').config();
+
 var config = {
   server: "localhost",
   authentication: {
     type: "default",
     options: {
-      userName: "ejemplo",
-      password: "ejemplo1234*",
+      userName: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
     },
   },
   options: {
     // If you are on Microsoft Azure, you need encryption:
     encrypt: false,
     //port: 1433,
-    database: "Northwind",
+    database: process.env.DATABASE_NAME,
   },
 };
 
